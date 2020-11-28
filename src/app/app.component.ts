586c630510c77cb1b0b2e6d36a16028ff78bcf50
Imran Remtulla <iremtulla15@gmail.com>
2020-11-27 22:33:18 -0500

    Added meta tags for SEO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { Router, RouterOutlet } from '@angular/router';
 import { fader } from './route-animations'
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -12,12 +13,17 @@ export class AppComponent implements OnInit {
   title = 'Imran Remtulla';
   hideNav = false
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private titleService: Title, private metaService: Meta) { }
 
   ngOnInit() {
     this.router.events.subscribe(event => {
       this.hideNav = (this.router.url == '/' || this.router.url == '/home')
     })
+    this.titleService.setTitle(this.title);
+    this.metaService.addTags([
+      {name: 'keywords', content: 'Imran, Remtulla, Resume, CV, Profile'},
+      {name: 'description', content: 'Computer Science student with a passion for learning and an interest in Web and App development.'}
+    ]);
   }
 
   prepareRoute(outlet: RouterOutlet) {
