0379d243845071302becef9ee360b417fc836394
Imran Remtulla <iremtulla15@gmail.com>
2021-01-01 17:31:57 +0300

    Upgrade to Angular 11

--- src/app/sticky-bar/sticky-bar.component.spec.ts
+++ src/app/sticky-bar/sticky-bar.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { StickyBarComponent } from './sticky-bar.component';
 
@@ -6,7 +6,7 @@ describe('StickyBarComponent', () => {
   let component: StickyBarComponent;
   let fixture: ComponentFixture<StickyBarComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ StickyBarComponent ]
     })
