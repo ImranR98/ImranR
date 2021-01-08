951a76bea1fc77404903f02d830f33ce65fa73e1
Imran Remtulla <iremtulla15@gmail.com>
2021-01-08 17:17:33 +0300

    Added Skills

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { MediaChange, MediaObserver } from '@angular/flex-layout';
+import { Subscription } from 'rxjs';
 
 /*
 TODO:
-- Add Skills
 - Add Social icons in About
 - Integrate # anchor tags if possible
 - Separate sections into components for maintainability
@@ -15,7 +16,7 @@ TODO:
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
   imageLoaded: boolean = false
 
@@ -23,7 +24,26 @@ export class HomeComponent implements OnInit {
   showCaret = false
   overrideShowCaret = false
 
-  constructor() { }
+  subscriptions: Subscription[] = []
+  columnNum: number = 8
+
+  constructor(media: MediaObserver) {
+    this.subscriptions.push(media.asObservable().subscribe((change: MediaChange[]) => {
+      if (change[0].mqAlias == 'xs') {
+        this.columnNum = 3
+      } else if (change[0].mqAlias == 'sm') {
+        this.columnNum = 6
+      } else if (change[0].mqAlias == 'md') {
+        this.columnNum = 8
+      } else if (change[0].mqAlias == 'lg') {
+        this.columnNum = 12
+      } else if (change[0].mqAlias == 'xl') {
+        this.columnNum = 16
+      } else {
+        this.columnNum = 20
+      }
+    }))
+  }
 
   sleep(timeout: number): Promise<void> {
     return new Promise((resolve, reject) => {
@@ -38,7 +58,6 @@ export class HomeComponent implements OnInit {
       threshold: 0.5
     }
     let observer = new IntersectionObserver((intersection) => {
-      console.log(intersection)
       if (intersection[0]?.isIntersecting || intersection[0]?.boundingClientRect.y < 0) target.classList.add('show')
       else target.classList.remove('show')
     }, options)
@@ -50,14 +69,14 @@ export class HomeComponent implements OnInit {
       if (!this.imageLoaded) this.loaded()
     }, 5000)
     window.addEventListener('scroll', this.scroll, true)
+  }
 
+  ngAfterViewInit() {
     let fadeIntoViewElements = document.getElementsByClassName('fadeIntoView')
     for (let i = 0; i < fadeIntoViewElements.length; i++) {
       this.fadeIntoView(fadeIntoViewElements[i])
     }
   }
-
-
 
   scroll() {
     if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
@@ -109,4 +128,138 @@ export class HomeComponent implements OnInit {
     'interested in Web and App development.',
     'located in Toronto 🇨🇦.'
   ]
+
+  skillTree = [
+    {
+      title: 'Web',
+      skills: [
+        {
+          image: '../../assets/images/nodejs.png',
+          title: 'Node.js'
+        },
+        {
+          image: '../../assets/images/angular.png',
+          title: 'Angular'
+        },
+        {
+          image: '../../assets/images/html.png',
+          title: 'HTML'
+        },
+        {
+          image: '../../assets/images/css.png',
+          title: 'CSS'
+        },
+        {
+          image: '../../assets/images/javascript.png',
+          title: 'JavaScript'
+        },
+        {
+          image: '../../assets/images/typescript.png',
+          title: 'TypeScript'
+        }
+      ]
+    },
+    {
+      title: 'Mobile',
+      skills: [
+        {
+          image: '../../assets/images/flutter.png',
+          title: 'Flutter'
+        }
+      ]
+    },
+    {
+      title: 'Database',
+      skills: [
+        {
+          image: '../../assets/images/mongodb.png',
+          title: 'MongoDB'
+        },
+        {
+          image: '../../assets/images/mysql.png',
+          title: 'MySQL'
+        },
+        {
+          image: '../../assets/images/postgresql.png',
+          title: 'PostgreSQL'
+        },
+      ]
+    },
+    {
+      title: 'Tools',
+      skills: [
+        {
+          image: '../../assets/images/sublime.png',
+          title: 'Sublime'
+        },
+        {
+          image: '../../assets/images/vs.png',
+          title: 'VS'
+        },
+        {
+          image: '../../assets/images/vscode.png',
+          title: 'VS Code'
+        }
+      ]
+    },
+    {
+      title: 'Platforms',
+      skills: [
+        {
+          image: '../../assets/images/apigee.png',
+          title: 'Apigee'
+        },
+        {
+          image: '../../assets/images/concourse.png',
+          title: 'Concourse'
+        },
+        {
+          image: '../../assets/images/heroku.png',
+          title: 'Heroku'
+        },
+        {
+          image: '../../assets/images/jira.png',
+          title: 'Jira'
+        },
+        {
+          image: '../../assets/images/pcf.png',
+          title: 'PCF'
+        },
+        {
+          image: '../../assets/images/servicenow.png',
+          title: 'ServiceNOW'
+        }
+      ]
+    },
+    {
+      title: 'Other',
+      skills: [
+        {
+          image: '../../assets/images/bash.png',
+          title: 'Bash'
+        },
+        {
+          image: '../../assets/images/cpp.png',
+          title: 'C++'
+        },
+        {
+          image: '../../assets/images/git.png',
+          title: 'Git'
+        },
+        {
+          image: '../../assets/images/java.png',
+          title: 'Java'
+        },
+        {
+          image: '../../assets/images/linux.png',
+          title: 'Linux'
+        },
+        {
+          image: '../../assets/images/photoshop.png',
+          title: 'Photoshop'
+        }
+      ]
+    }
+  ]
+
 }
