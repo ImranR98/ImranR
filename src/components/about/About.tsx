a43ae532662c07129b06d51d2d1872d74c6eca0e
Imran Remtulla <iremtulla15@gmail.com>
2021-01-28 00:14:07 +0300

    Progress

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,6 +1,8 @@
 import './About.css'
 import { useState } from 'react'
 import { useScrollPosition } from '@n8tb1t/use-scroll-position'
+import me from '../../assets/images/me.jpg'
+import down from '../../assets/icons/down.svg'
 
 const About = () => {
     const [compact, setCompact] = useState(false)
@@ -13,11 +15,15 @@ const About = () => {
     return (
         <div className={"about flexColumn centerFlex stdPadding" + (compact ? " compact" : "")}>
             <div className="flexRow centerFlex inner">
-                <img alt="Imran Remtulla" src="/images/me.jpg" className="me" />
+                <img alt="Imran Remtulla" src={me} className="me" />
                 <div className="flexColumn stdPadding">
                     <p>Hi</p>
                     <h1>I'm <span className="accent">Imran Remtulla</span></h1>
+                    <p className="typewriter">A Computer Science student.</p>
                 </div>
+            </div>
+            <div className="down">
+                <img alt="Scroll down" src={down} className={(compact ? " fadeOut" : "")} />
             </div>
         </div >
     );
