6f7a8c712b9528e18d33e8d299ee432ffd727511
Imran Remtulla <iremtulla15@gmail.com>
2021-01-29 19:36:46 +0300

    Item recognizes if it is out of view (will be used for animation)

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -1,11 +1,20 @@
 import './Item.css'
+import { useRef, useState } from 'react'
+import { useScrollPosition } from '@n8tb1t/use-scroll-position'
 
 const Item = ({ image, title, subtitle, children, BG, imageBG }: { image: string, title: string, subtitle: string, children: any, BG: string, imageBG: string }) => {
+    const [elementPosition, setElementPosition] = useState({ x: 0, y: 0 })
+    const elementRef = useRef(null)
 
+    useScrollPosition(
+        ({ currPos }) => setElementPosition(currPos), [], elementRef as any
+    )
+
+    const isReasonablyVisible = (offset: number = 40) => ((elementPosition.y + offset)- Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)) <= 0
 
     return (
-        <div className="item" style={{ backgroundColor: `#${BG}`}}>
-            <div className="logo" style={{ backgroundColor: `#${imageBG}`}}>
+        <div ref={elementRef} className={"item" + (!isReasonablyVisible() ? " belowScroll" : "")} style={{ backgroundColor: `#${BG}` }}>
+            <div className="logo" style={{ backgroundColor: `#${imageBG}` }}>
                 <img alt={title} src={image} />
             </div>
             <div className="data stdPadding">
