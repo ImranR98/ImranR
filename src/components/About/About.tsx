6ddca310cb6436177b55f68e47f2049d6c044a7d
Imran Remtulla <iremtulla15@gmail.com>
2021-03-07 01:35:56 +0300

    Replaced typewriter library with a maintained one

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import { useScrollPosition } from '@n8tb1t/use-scroll-position'
 import me from '../../assets/images/me.jpg'
 import down from '../../assets/icons/down.svg'
-import Typewriter from 'typewriter-effect';
+import { Pace, WindupChildren } from 'windups'
 import email from '../../assets/icons/email.svg'
 import github from '../../assets/icons/github.svg'
 import linkedin from '../../assets/icons/linkedin.svg'
@@ -17,6 +17,19 @@ const About = () => {
         if (temp !== compact) setCompact(temp)
     }, [compact])
 
+    const strings = [
+        'Comp. Sci. Student with a \nPassion for Learning',
+        'Logical and efficient problem solver',
+        'Perfectionist who priorities UX',
+        'Responsible, Reliable, and Adaptable',
+        'Big fan of Open-Source Software'
+    ]
+    const [activeString, setActiveString] = useState(0)
+
+    setTimeout(() => {
+        activeString === strings.length - 1 ? setActiveString(0) : setActiveString(activeString + 1)
+    }, 5000)
+
     return (
         <div id="about" className={"about flexColumn centerFlex stdPadding" + (compact ? " compact" : "")}>
             <div className="flexRow centerFlex inner">
@@ -24,21 +37,10 @@ const About = () => {
                 <div className="flexColumn intro">
                     <p>Hi</p>
                     <h1>I'm <span className="accent">Imran Remtulla</span></h1>
-                    <Typewriter
-                        onInit={(typewriter) => { }}
-                        options={{
-                            strings: [
-                                'Comp. Sci. Student with a \nPassion for Learning',
-                                'Logical and efficient problem solver',
-                                'Perfectionist who priorities UX',
-                                'Responsible, Reliable, and Adaptable',
-                                'Big fan of Open-Source Software'
-                            ],
-                            autoStart: true,
-                            loop: true,
-                            delay: 100,
-                            deleteSpeed: 15
-                        }} />
+
+                    <WindupChildren>
+                        <div className="typewriter"><Pace ms={50}>{strings[activeString]}</Pace></div>
+                    </WindupChildren>
                     <div className="contact">
                         <a href="mailto:iremtulla15@gmail.com" target="_blank" rel="noreferrer"><img alt="Email" src={email} className="icon"></img></a>
                         <a href="https://github.com/ImranR98" target="_blank" rel="noreferrer"><img alt="GitHub" src={github} className="icon"></img></a>
